3cb366c48a2fc8e3039aa43b78a4f040896f7ab9
JeissonGerardo <JeissonGerardo@Jeisson-PC>
2015-03-10 21:12:31 -0500

    cambio a proyecto vias

--- module/Login/src/Login/Model/Entity/ProyectoVias.php
+++ module/Login/src/Login/Model/Entity/ProyectoVias.php
@@ -69,7 +69,7 @@ class ProyectoVias {
     /**
      * @var \Login\Model\Entity\Proyecto
      *
-     * @ORM\ManyToOne(targetEntity="Login\Model\Entity\Proyecto",cascade={"persist"})
+     * @ORM\ManyToOne(targetEntity="Login\Model\Entity\Proyecto", cascade={"persist"})
      * @ORM\JoinColumns({
      *   @ORM\JoinColumn(name="proyecto_id", referencedColumnName="proyecto_id")
      * })
